a3e73d302577e1d6b0394b3fd414668f9f2d8485
Smokey01966 <82545255+Smokey01966@users.noreply.github.com>
2023-05-31 10:16:11 -0400

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfTimes) {
+    let result="" ;
+    if(numberOfTimes < 0){
+        result="ERROR" ;
+        return result ;
+    }
+    for(let i = 0; i < numberOfTimes; i++){
+        result += string ;
+    }
+    return result ;
 };
 
 // Do not edit below this line
