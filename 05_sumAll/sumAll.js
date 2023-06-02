0110c84f0841ab40c6c3d4d477758da172933202
Smokey01966 <82545255+Smokey01966@users.noreply.github.com>
2023-06-02 09:35:20 -0400

    Compleat exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let result = 0;
+    let top = 0;
+    let bottom = 0;
+    
+    /* check inputs for errors */
+    if (typeof num1 !== "number" || typeof num2 !=="number" || num1 < 0 || num2 < 0){
+        return "ERROR" ;
+    }
+   
+   /* decide wich input should be top and bottom */
+    if (num1 > num2){
+        top = num1
+        bottom = num2
+    } else {
+        top = num2 ;
+        bottom = num1 ;
+    }
+    
+    /* sum all values from bottom - top inclusive */
+    for (let i = bottom; i < top+1; i++){
+        result += i;
+    }
+    return result;
 
 };
 
