309094005f38469f1ea99c95ed00aa2e8036bc7c
Smokey01966 <82545255+Smokey01966@users.noreply.github.com>
2023-06-02 09:49:38 -0400

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result = "ERROR" ;
+    if (year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0)){
+        result = true ;
+    } else {
+        result = false ;
+    }
+    return result ;
 };
 
 // Do not edit below this line
