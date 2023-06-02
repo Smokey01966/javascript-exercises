22ddb93e2bf44c2c689abd813213213764968735
Smokey01966 <82545255+Smokey01966@users.noreply.github.com>
2023-06-02 09:12:08 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let result=["ERROR"] ;
+    result = array.filter(item => !args.includes(item)) ;    
+    return result
 };
 
 // Do not edit below this line
