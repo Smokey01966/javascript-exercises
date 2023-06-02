8c99c221afb5ea4ac676218562b7b35dc991c041
Smokey01966 <82545255+Smokey01966@users.noreply.github.com>
2023-06-02 10:07:14 -0400

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let result = "ERROR" ;
+  result = Math.round(((tempF - 32) * (5/9)) * 10) /10 ;
+  return result ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let result = "ERROR" ;
+  result = Math.round(((tempC * (9/5) ) + 32) * 10) /10 ;
+  return result ;
 };
 
 // Do not edit below this line
